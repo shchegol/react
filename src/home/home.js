02c7d0ceead2bbfcc90caf77026bb0c21dc9a949
Alexander Shchegol <zelenzoom@gmail.com>
2017-07-31 23:12:08 +0300

    state and lifecycle

--- src/home/home.js
+++ src/home/home.js
@@ -4,15 +4,78 @@ import ReactDOM from 'react-dom';
 export default {
 
     start() {
-        class Welcome extends React.Component {
+        const location = document.body;
+
+        // function tick() {
+        //
+        //     const element = (
+        //         <div>
+        //             <h1>Hello, world!</h1>
+        //             <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        //         </div>
+        //     );
+        //
+        //     ReactDOM.render(
+        //         element, location
+        //     )
+        // }
+
+        // function Clock(props) {
+        //     return (
+        //         <div>
+        //             <h1>Hello, world!</h1>
+        //             <h2>
+        //                 It is {props.date.toLocaleTimeString()}.
+        //             </h2>
+        //         </div>
+        //     );
+        // }
+        //
+        // function tick() {
+        //     ReactDOM.render(
+        //         <Clock date={new Date()}/>,
+        //         location
+        //     );
+        // }
+
+        class Clock extends React.Component {
+            constructor(props) {
+                super(props);
+                this.state = {date: new Date()};
+            }
+
+            componentDidMount() {
+                this.timerID = setInterval(
+                    () => this.tick(),
+                    1000
+                )
+            }
+
+            componentWillUnmount() {
+                clearInterval(this.timerID);
+            }
+
+            tick() {
+                this.setState({
+                    date: new Date()
+                })
+            }
+
             render() {
-                return <h1>Hello, {this.props.name}</h1>
+                return (
+                    <div>
+                        <h1>Hello, world!</h1>
+                        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+                    </div>
+                )
             }
         }
 
-        const element = <Welcome name="Sara" />;
-        const location = document.querySelector('body');
+        ReactDOM.render(
+            <Clock/>,
+            location
+        );
 
-        ReactDOM.render(element, location);
+
     }
 }
